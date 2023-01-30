e4c6860113a653e7252f662d17969cfdd3db8caf
rahul <rdtech2002@gmail.com>
2023-01-30 16:14:41 +0530

    .

--- day-009-29-jan-loops-and-pattern-programs/CODES/Question_03.java
+++ day-009-29-jan-loops-and-pattern-programs/CODES/Question_03.java
@@ -1,20 +1,25 @@
-public class Question_03 {
-
-  public static void main(String[] args) {
-    for (int i = 0; i < 14; i++) {
-      for (int j = 0; j < 14; j++) {
-        if (i == 0 || j == 0 || i == 13 || j == 13) {
-          System.out.print("*");
-        }
-        for(int k =0;k<=j;k++){
-            System.out.print("*");
-        }
-        System.out.println();
-      }
-    //   for (int j = 5; j >= 0; j--) {
-        
-    //   }
-      System.out.println();
-    }
-  }
+public class Question_03
+{
+	public static void main(String[] args) {
+	    int n = 14;
+	    for(int i=0;i<=n;i++)
+	    {
+	        for(int j=0;j<=n;j++)
+	        {
+	            if(i==0&&j<(n-1)||i==(n-1)&&j<(n-1)||j==0&&i<(n-1)||j==(n-1)&&i<=(n-1)||i==1&&j!=(n-1)/2&&i<(n/4)&&j<(n-1)||
+	            i==2&&j!=(n-1)/2&&j<(n-1)&&j!=5&&j!=7||i==3&&j!=(n-1)/2&&j<(n-1)&&j!=6&&j!=5&&j!=4&&j!=7&&j!=8
+	            ||i==4&&j!=(n-1)/2&&j<(n-1)&&j!=3&&j!=5&&j!=4&&j!=7&&j!=8&&j!=9||
+	            i==5&&j!=(n-1)/2&&j<(n-1)&&j!=3&&j!=5&&j!=4&&j!=7&&j!=8&&j!=9&&j!=2&&j!=10||
+	            i==6&&j!=(n-1)/2&&j<(n-1)&&j!=3&&j!=5&&j!=4&&j!=7&&j!=8&&j!=9&&j!=2&&j!=10&&j!=1&&j!=11)
+	            {
+	                System.out.print("*");
+	            }
+	            else
+	            {
+	                System.out.print(" ");
+	            }
+	        }
+	        System.out.println();
+	    }
+	}
 }
