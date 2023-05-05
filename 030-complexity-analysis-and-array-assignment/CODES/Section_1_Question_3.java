86b4dbb4a78d8d8a7489ef4040aa1bcc1484cfc5
Rahul Dutta <rdtech2002@gmail.com>
2023-05-05 12:53:00 +0530

    .

---
+++ 030-complexity-analysis-and-array-assignment/CODES/Section_1_Question_3.java
@@ -0,0 +1,32 @@
+/*
+Write a program to calculate the maximum element in the array.
+Input 1: arr[] = {34,21,54,65,43}
+Output 1: 65
+Input 1: arr[] = {4,3,6,7,1}
+Output 1: 7
+*/
+// time complexity : o(n)
+// space complexity : O(1)
+
+public class Section_1_Question_3 {
+
+  public static void getMaxElement(int[] arr) {
+    int max = 0;
+    for (int i : arr) {
+      if (i > max) {
+        max = i;
+      }
+    }
+    System.out.println("Max element in the array is : " + max);
+  }
+
+  // driver code
+  public static void main(String[] args) {
+    // test case - 1
+    int arr[] = { 34, 21, 54, 65, 43 };
+    getMaxElement(arr);
+    // test case - 2
+    int arr2[] = { 4, 3, 6, 7, 1 };
+    getMaxElement(arr2);
+  }
+}
